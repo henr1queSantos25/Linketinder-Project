95825b99e44d7fd5921d7cd3fee92c5beb103409
Henrique Oliveira <202410486@uesb.edu.br>
2026-03-15 20:08:54 -0300

    feat: adiciona campos de telefone e linkedin ao modelo e formulario de candidato

--- frontend/dist/models/Candidato.d.ts
+++ frontend/dist/models/Candidato.d.ts
@@ -2,5 +2,7 @@ import type { IPessoa } from "./Pessoa.js";
 export interface ICandidato extends IPessoa {
     cpf: string;
     idade: number;
+    telefone: string;
+    linkedin: string;
 }
 //# sourceMappingURL=Candidato.d.ts.map
--- frontend/src/models/Candidato.ts
+++ frontend/src/models/Candidato.ts
@@ -3,4 +3,6 @@ import type { IPessoa } from "./Pessoa.js";
 export interface ICandidato extends IPessoa {
     cpf: string;
     idade: number;
+    telefone: string;
+    linkedin: string;
 }
